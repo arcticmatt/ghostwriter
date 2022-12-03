68367a7fdf7db75a3715bdc5a13185393e4b1df6
Matt Lim <mlimcollege@gmail.com>
2022-12-02 20:24:08 -0800

    Add new post form

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -15,11 +15,15 @@ export const loader = async () => {
 };
 
 export default function Posts() {
+  // @ts-ignore
   const { posts } = useLoaderData() as LoaderData;
   console.log(posts);
   return (
     <main>
       <h1>Posts</h1>
+      <Link to="admin" className="text-red-600 underline">
+        Admin
+      </Link>
       <ul>
         {posts.map((post) => (
           <li key={post.slug}>
