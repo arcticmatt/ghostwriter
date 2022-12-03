f11fd60356bca6435c2bb263effbd0fbfaf9651f
Matt Gabor <5590876+mattgabor@users.noreply.github.com>
2022-12-03 15:29:08 -0800

    Added openAI createCompletion example

--- scripts/openai.js
+++ scripts/openai.js
@@ -2,11 +2,32 @@ const { Configuration, OpenAIApi } = require("openai");
 
 async function run() {
   const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+    apiKey: "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",
   });
   const openai = new OpenAIApi(configuration);
-  const response = await openai.listEngines();
-  console.log(response.data);
+
+  const contentType = "haiku";
+  const about = "pickles";
+  const personality = "Sylvia Plath";
+
+  const prompt = `Write a ${contentType} about ${about} in the style of ${personality}`;
+
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt,
+      // safety check so we don't get billed like crazy
+      max_tokens: 50,
+    });
+    console.log(completion.data.choices[0].text);
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      console.log(error.message);
+    }
+  }
 }
 
 run();
