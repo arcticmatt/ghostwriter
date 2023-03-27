0889175250ddd8261826d10129892420cf2adbca
Matt Gabor <5590876+mattgabor@users.noreply.github.com>
2023-03-27 16:14:26 -0700

    feat: upgrade to gpt-4 API

--- app/api/getGeneratedContent.ts
+++ app/api/getGeneratedContent.ts
@@ -20,18 +20,19 @@ async function getGeneratedContent(
 
   try {
     // Parameters taken from https://beta.openai.com/playground/p/default-chat
-    const completion = await openAI.createCompletion({
-      best_of: 1,
-      frequency_penalty: 0,
-      model: "text-davinci-003",
-      presence_penalty: 0.6,
-      prompt,
+    const completion = await openAI.createChatCompletion({
+      // best_of: 1,
+      // frequency_penalty: 0,
+      model: "gpt-4",
+      // presence_penalty: 0.6,
+      messages: [{ role: "user", content: prompt }],
       // safety check so we don't get billed like crazy
       max_tokens: 500,
-      temperature: 0.9,
-      top_p: 1,
+      // temperature: 0.9,
+      // top_p: 1,
     });
-    response = completion.data.choices[0].text;
+
+    response = completion.data.choices[0].message.content;
   } catch (error) {
     if (error.response) {
       console.log(error.response.status);
